ed758eeb098b4f764ff73857fe7bd0cd34109fb0
bjguoweilong <guoweilong@jd.com>
2018-12-26 13:16:45 +0800

    fix copryright and license

--- raft_fsm_leader.go
+++ raft_fsm_leader.go
@@ -1,5 +1,4 @@
 // Copyright 2015 The etcd Authors
-// Modified work copyright 2018 The tiglabs Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
